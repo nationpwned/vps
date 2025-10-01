2d8ab92e482082bd4b9e208cfea7bd1381bc41ed
nationpwned <17819957+nationpwned@users.noreply.github.com>
2025-10-01 16:45:58 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,9 +6,10 @@ if [ "$EUID" -ne 0 ]; then
   echo "Please run as root (e.g., with sudo su)" >&2
   exit 1
 fi
-# Define the domain for your marzban instance
-read -p "Enter your domain for marzban: " DOMAIN
+# Define the domain for your pasarguard instance
+read -p "Enter your domain for pasarguard: " DOMAIN
 read -p "Enter your email for SSL certificate (inibudi@daouse.com): " MAIL
+read -p "Enter your pasarguard admin username (admin): " ADMIN
 
 
 # Update the system and install necessary packages
@@ -48,160 +49,51 @@ fi
 
 sysctl -p >/dev/null 2>&1
 
-if command -v marzban >/dev/null 2>&1; then
-  echo "Existing marzban installation detected. Uninstalling..."
-  marzban uninstall
+if command -v pasarguard >/dev/null 2>&1; then
+  echo "Existing pasarguard installation detected. Uninstalling..."
+  pasarguard uninstall
 fi
 
-bash -c "$(curl -sL https://github.com/nationpwned/vps/raw/refs/heads/marzban/marzban)" @ install
+bash -c "$(curl -sL https://github.com/nationpwned/vps/raw/refs/heads/pasarguard/pasarguard)" @ install
 sleep 50
 
-marzban cli admin create --sudo
+pasarguard cli admins create $ADMIN
 
-[ -f /$HOME/reality.txt ] && rm -f /$HOME/reality.txt
-[ -f /$HOME/shortIds.txt ] && rm -f /$HOME/shortIds.txt
-[ -f /$HOME/xray_uuid.txt ] && rm -f /$HOME/xray_uuid.txt
 
-# Generate Reality keys
-echo "Generating Reality keys..."
-docker exec marzban-marzban-1 xray x25519 genkey > /$HOME/reality.txt
-PRIVATE_KEY=$(grep -oP 'Private key: \K\S+' /$HOME/reality.txt)
-PUBLIC_KEY=$(grep -oP 'Public key: \K\S+' /$HOME/reality.txt)
+# Install Certificate using acme.sh
+bash -c "curl https://get.acme.sh | sh -s email=$MAIL"
 
-# Generate shortIds
-echo "Generating shortIds..."
-openssl rand -hex 8 > /$HOME/shortIds.txt
-SHORTIDS=$(cat /$HOME/shortIds.txt)
+bash -c "~/.acme.sh/acme.sh --issue --force --standalone -d \"$DOMAIN\" --fullchain-file \"/var/lib/pasarguard/fullchain.pem\" --key-file \"/var/lib/pasarguard/key.pem\""
+pasarguard down
 
-# Generating uuid for Reality
-echo "Generating UUID for Reality..."
-if ! docker ps | grep -q marzban-marzban-1; then
-  echo "marzban container not running! Exiting."
-  exit 1
-fi
-docker exec marzban-marzban-1 xray uuid > /$HOME/xray_uuid.txt
-XRAY_UUID=$(cat /$HOME/xray_uuid.txt)
-if [[ -z "$XRAY_UUID" ]]; then
-  echo "Failed to generate UUID. Exiting."
-  exit 1
-fi
+# Set proper permissions
+chmod 600 "/var/lib/pasarguard/key.pem"
+chmod 644 "/var/lib/pasarguard/fullchain.pem"
 
-# Check if certificate already exists
-rm -Rf /var/lib/marzban/certs >/dev/null 2>&1 || true
-if [[ -f "/var/lib/marzban/certs/fullchain.pem" && -f "/var/lib/marzban/certs/key.pem" ]]; then
-    echo "SSL certificate already exists. Skipping certificate installation."
-else
-    # Install Certificate using acme.sh
-    bash -c "curl https://get.acme.sh | sh -s email=$MAIL"
-    mkdir -p /var/lib/marzban/certs
-    bash -c "~/.acme.sh/acme.sh --issue --force --standalone -d \"$DOMAIN\" --fullchain-file \"/var/lib/marzban/certs/fullchain.pem\" --key-file \"/var/lib/marzban/certs/key.pem\""
-    marzban down
-
-    # Set proper permissions
-    chmod 600 "/var/lib/marzban/certs/key.pem"
-    chmod 644 "/var/lib/marzban/certs/fullchain.pem"
-fi
-
-wget -O /opt/marzban/.env https://github.com/nationpwned/vps/raw/refs/heads/marzban/env
+wget -O /opt/pasarguard/.env https://github.com/nationpwned/vps/raw/refs/heads/pasarguard/env
 # Download docker-compose.yml
-wget -O /opt/marzban/docker-compose.yml https://github.com/nationpwned/vps/raw/refs/heads/marzban/docker-compose.yml
+wget -O /opt/pasarguard/docker-compose.yml https://github.com/nationpwned/vps/raw/refs/heads/pasarguard/docker-compose.yml
 
 # Download nginx.conf
-wget -O /opt/marzban/nginx.conf https://github.com/nationpwned/vps/raw/refs/heads/marzban/nginx.conf
+wget -O /opt/pasarguard/nginx.conf https://github.com/nationpwned/vps/raw/refs/heads/pasarguard/nginx.conf
 # Replace placeholders in nginx.conf with user input
-sed -i "s/server_name \$DOMAIN;/server_name $DOMAIN;/" /opt/marzban/nginx.conf
+sed -i "s/server_name \$DOMAIN;/server_name $DOMAIN;/" /opt/pasarguard/nginx.conf
 
 # Download xray_config.json
-wget -O /var/lib/marzban/xray_config.json https://github.com/nationpwned/vps/raw/refs/heads/marzban/xray_config.json
+wget -O /var/lib/pasarguard/xray_config.json https://github.com/nationpwned/vps/raw/refs/heads/pasarguard/xray_config.json
 
-sed -i "s/YOUR_UUID/$XRAY_UUID/" /var/lib/marzban/xray_config.json
+sed -i "s/YOUR_UUID/$XRAY_UUID/" /var/lib/pasarguard/xray_config.json
 
-# Download the subscribers marzban
-mkdir -p /var/lib/marzban/templates/subscription/
-wget -N -P /var/lib/marzban/templates/subscription/ https://github.com/nationpwned/vps/raw/refs/heads/marzban/index.html
-
-# Firewall configuration
-echo "Configuring firewall..."
-ufw allow 8000/tcp
-ufw allow 80/tcp
-ufw allow 443/tcp
-ufw allow 8443/tcp
-ufw allow 8444/tcp
-ufw allow 8445/tcp
-ufw allow 8446/tcp
-ufw allow 8447/tcp
-ufw allow 8448/tcp
-ufw allow 9443/tcp
-ufw allow 10443/tcp
-ufw allow 22/tcp
-ufw allow 2222/tcp
-ufw allow 2021/tcp
-ufw allow 2022/tcp
-ufw allow 2023/tcp
-ufw allow 2024/tcp
-ufw allow 2025/tcp
-ufw allow 51820/tcp
-ufw allow 51821/tcp
-ufw allow 51822/tcp
-ufw allow 51823/tcp
-ufw allow 51824/tcp
-ufw allow 51825/tcp
-ufw allow 8443/tcp
-ufw allow 9443/tcp
-ufw allow 62050/tcp
-ufw allow 62051/tcp
-
-ufw --force enable
+# Download the subscribers pasarguard
+mkdir -p /var/lib/pasarguard/templates/subscription/
+wget -N -P /var/lib/pasarguard/templates/subscription/ https://github.com/nationpwned/vps/raw/refs/heads/pasarguard/index.html
 
 # Cloudflare Warp installation
 echo "Installing Cloudflare Warp..."
-docker compose -f /opt/marzban/docker-compose.yml up -d
+docker compose -f /opt/pasarguard/docker-compose.yml up -d
 
-# Ensure /opt/marzban/wgcf directory is fresh
-if [ -d /opt/marzban/wgcf ]; then
-  rm -rf /opt/marzban/wgcf
-fi
-mkdir -p /opt/marzban/wgcf
-
-# Download wgcf binary
-WGCF_LATEST_URL=$(curl -s https://api.github.com/repos/ViRb3/wgcf/releases/latest | grep "browser_download_url" | grep "linux_amd64" | cut -d '"' -f 4)
-wget "$WGCF_LATEST_URL" -O /usr/local/bin/wgcf
-
-chmod +x /usr/local/bin/wgcf
-# Configure Cloudflare Warp
-echo "Configuring Cloudflare Warp..."
-wgcf register --accept-tos || true > /dev/null 2>&1
-wgcf generate
-mv wgcf-profile.conf /opt/marzban/wgcf/wg0.conf
-mv wgcf-account.toml /opt/marzban/wgcf/
-sed -i -E 's/, [0-9a-f:]+\/128//; s/, ::\/0//' /opt/marzban/wgcf/wg0.conf
-sleep 3
-docker restart wgcf-warp
-sleep 5
-
-echo "==============================================="
-# Check Cloudflare Warp status (Cloudflare and ip-api.com)
-if docker exec wgcf-warp curl -s https://www.cloudflare.com/cdn-cgi/trace | grep -q "warp=on"; then
-    echo "Cloudflare Warp is ON (Cloudflare trace)."
-else
-    echo "Cloudflare Warp is OFF (Cloudflare trace)."
-fi
-
-if docker exec wgcf-warp curl -s http://ip-api.com/json | grep -q 'Cloudflare WARP'; then
-    echo "Cloudflare Warp is ON (ip-api.com check)."
-else
-    echo "Cloudflare Warp is OFF or not detected by ip-api.com."
-fi
-
-echo "==============================================="
-echo "private key: $PRIVATE_KEY"
-echo "public key: $PUBLIC_KEY"
-echo "ShortIds: $SHORTIDS"
-echo "UUID: $XRAY_UUID"
-echo "==============================================="
-
-echo "marzban installation and configuration completed successfully!"
-echo "You can access marzban at https://$DOMAIN"
+echo "pasarguard installation and configuration completed successfully!"
+echo "You can access pasarguard at https://$DOMAIN"
 echo "Make sure to configure your Xray clients with the provided Reality keys and UUID."
 echo "==============================================="
 
